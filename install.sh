a78667a63bf8e90c9c21ad87b8de602621a6cdf7
Satyam Gothi <41023447+LifeHack3r@users.noreply.github.com>
2020-05-02 22:01:52 +0000

    Added Dir Checks

--- install.sh
+++ install.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 
-mkdir ~/tools
+if [[ ! -d ~/tools ]]
+then
+	mkdir ~/tools
+else
+	printf "tools folder present...\n\n"
+fi
 cd ~/tools
-git clone https://github.com/dark-warlord14/LinkFinder
 
+if [[ ! -d ~/tools/LinkFinder ]]
+then
+	git clone https://github.com/dark-warlord14/LinkFinder
+else
+	printf "LinkFinder already present in tools folder...!\n\n"
+fi
 apt install wget -y
 cd LinkFinder
 
 pip3 install -r requirements.txt
 python3 setup.py install
+
+
+sudo chmod +x ~/JSScanner/script.sh
+
+
+echo "alias jsscanner='$HOME/JSScanner/script.sh'" >> ~/.bash_aliases
+source ~/.bash_aliases
 
 echo "All set bro"
